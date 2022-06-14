d6b5c735cbf29ae54d40763081db272f7740769d
Yongxiang Liang <tanekliang@gmail.com>
2022-06-15 01:40:29 +0800

    split gentoo_deploy script

---
+++ README.md
@@ -0,0 +1,27 @@
+# tinderbox
+
+## install
+
+gentoo:
+
+```bash
+bash <(curl -s https://raw.githubusercontent.com/liangyongxiang/tinderbox-cluster-deploy/master/gentoo_deploy.sh)
+```
+
+debian:
+
+```bash
+: # TODO
+```
+
+## deploy
+
+```bash
+cd /var/tmp/tinderbox
+
+export IRC_CHANNEL_NAME="yourname-bb"
+export IRC_BOT_NAME="yournamebot-bb"
+
+./deploy.sh
+```
+
--- gentoo_prepare.sh
+++ gentoo_prepare.sh
@@ -2,11 +2,8 @@
 
 # Step 1: depends
 emerge-webrsync
-emerge --verbose --quiet dev-vcs/git app-misc/tmux dev-db/postgresql dev-python/pip
+emerge --verbose --quiet --update app-misc/tmux dev-vcs/git app-misc/tmux dev-db/postgresql dev-python/pip
 emerge --config dev-db/postgresql:14
 # TODO: systemd
 /etc/init.d/postgresql-* start
 git clone https://github.com/liangyongxiang/tinderbox-cluster-deploy /var/tmp/tinderbox
-
-cd /var/tmp/tinderbox
-source deploy.sh
