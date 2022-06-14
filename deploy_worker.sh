97c4de9c2f395926fe00e8160a7971cb6409cc21
Yongxiang Liang <tanekliang@gmail.com>
2022-06-15 07:09:42 +0800

    update deploy worker

--- deploy_worker.sh
+++ deploy_worker.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#
 
 set -x
 set -e
@@ -11,8 +10,14 @@ WORKER_PATH="${WORKER_BASEDIR}/${WORKER_NAME}"
 PASSWORD="${PASSWORD:-riscv}"
 MASTER_HOST="${MASTER_HOST:-localhost}"
 MASTER_PORT="${MASTER_PORT:-9989}"
-STAGE3_MIRROR="${STAGE3_MIRRORS:-https://gentoo.osuosl.org/}"
+STAGE3_MIRROR="${STAGE3_MIRROR:-https://gentoo.osuosl.org/}"
 STAGE3_FILENAME="${STAGE3_FILENAME:-stage3-amd64-openrc-20220612T170541Z.tar.xz}"
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+if [ ! -f "${STAGE3_FILENAME}" ]; then
+    wget "${STAGE3_MIRROR}/releases/amd64/autobuilds/current-stage3-amd64-openrc/${STAGE3_FILENAME}"
+fi
 
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
@@ -29,23 +34,25 @@ fi
 mkdir "${WORKER_PATH}"
 cd "${WORKER_PATH}"
 
-cp deploy_in_chroot.sh .
+cp "${SCRIPT_DIR}/deploy_in_chroot.sh" .
 chmod a+x deploy_in_chroot.sh
 
-wget "${STAGE3_MIRROR}/releases/amd64/autobuilds/current-stage3-amd64-openrc/${STAGE3_FILENAME}"
+tar xpf "${SCRIPT_DIR}/${STAGE3_FILENAME}" --numeric-owner --xattrs-include='*.*'
 
-tar xpf "${STAGE3_FILENAME}" --numeric-owner --xattrs-include='*.*'
-
-cp -L /etc/resolv.conf etc
 mkdir etc/portage/repos.conf
 cp usr/share/portage/config/repos.conf etc/portage/repos.conf/gentoo.conf
 
-mount -t proc /proc proc
-mount --rbind /sys sys
-mount --make-rslave sys
-mount --rbind /dev dev
-mount --make-rslave dev
-mount --bind /run run
-mount --make-slave run
-
-chroot "/mnt/${WORKER_NAME}" /bin/bash -c "WORKER_NAME=${WORKER_NAME} PASSWORD=${PASSWORD:-riscv} MASTER_HOST=${MASTER_HOST} MASTER_PORT=${MASTER_PORT} ./deploy_in_chroot.sh"
+bwrap \
+    --die-with-parent \
+    --bind      "${WORKER_PATH}" / \
+    --ro-bind   /etc/resolv.conf /etc/resolv.conf \
+    --tmpfs     /run \
+    --dev       /dev \
+    --bind      /sys /sys \
+    --proc      /proc \
+    --setenv    WORKER_NAME "${WORKER_NAME}" \
+    --setenv    PASSWORD "${PASSWORD}" \
+    --setenv    MASTER_HOST "${MASTER_HOST}" \
+    --setenv    MASTER_PORT "${MASTER_PORT}" \
+    --share-net \
+    /bin/bash --login /deploy_in_chroot.sh
