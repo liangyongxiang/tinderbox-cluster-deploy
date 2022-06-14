72e0d02d02adda83da74b788b24c96358f45abda
Yongxiang Liang <tanekliang@gmail.com>
2022-06-15 07:10:46 +0800

    fixed /var/db/repos/gentoo

--- deploy_in_chroot.sh
+++ deploy_in_chroot.sh
@@ -9,10 +9,11 @@ MASTER_HOST="${MASTER_HOST:-localhost}"
 MASTER_PORT="${MASTER_PORT:-9989}"
 
 echo 'ACCEPT_KEYWORDS="~amd64"' >> /etc/portage/make.conf
-chown -R portage:portage /var/db/repos/gentoo
 
 emerge-webrsync
-emerge -uUDN --with-bdeps=y @world
+chown -R portage:portage /var/db/repos/gentoo
+
+emerge -qvuUDN -j --with-bdeps=y @world
 emerge --verbose --quiet --noreplace dev-util/pkgcheck app-arch/zstd dev-lang/rust-bin dev-vcs/git dev-util/buildbot-worker
 
 cd /var/tmp
